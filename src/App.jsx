614fab05648dff53648aa52057718666e1aa2884
Utkarsh Bajpai <utkarshbajpai446@gmail.com>
2024-07-06 12:33:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/NavBar/Navbar'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Coin from './pages/Coin/Coin'
+import Footer from './components/Footer/Footer'
+
+
+const App = () => {
+  return (
+    <div className='app'>
+    <Navbar></Navbar>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/coin/:coinId' element={<Coin/>}/>
+    </Routes>
+   <Footer/>
+    </div>
+  )
+}
+
+export default App
